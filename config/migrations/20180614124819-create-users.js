57103bdea3c07c41994c4c7d09d79d6b5b3bc278
daliannyvieira <vieiradalianny@gmail.com>
2018-10-31 16:28:33 -0300

    improve json format

--- config/migrations/20180614124819-create-users.js
+++ config/migrations/20180614124819-create-users.js
@@ -7,17 +7,56 @@ module.exports = {
         primaryKey: true,
         type: DataTypes.INTEGER,
       },
-      name: {
-        allowNull: false,
+      ds_profile: {
         type: DataTypes.STRING,
       },
-      email: {
-        allowNull: false,
+      ds_email: {
         type: DataTypes.STRING,
         unique: true,
       },
-      password: {
-        allowNull: false,
+      ds_name: {
+        type: DataTypes.STRING,
+      },
+      ds_bio: {
+        type: DataTypes.STRING,
+      },
+      dt_birthday: {
+        type: DataTypes.DATE,
+      },
+      ds_avatar: {
+        type: DataTypes.STRING,
+      },
+      ds_country: {
+        type: DataTypes.STRING,
+      },
+      ds_city: {
+        type: DataTypes.STRING,
+      },
+      ds_address: {
+        type: DataTypes.STRING,
+      },
+      nr_latlong: {
+        type: DataTypes.STRING,
+      },
+      ds_skills: {
+        type: DataTypes.STRING,
+      },
+      ds_causes: {
+        type: DataTypes.STRING,
+      },
+      ds_areas: {
+        type: DataTypes.STRING,
+      },
+      ds_sdgs: {
+        type: DataTypes.STRING,
+      },
+      fl_is_allow_to_remote: {
+        type: DataTypes.BOOLEAN,
+      },
+      ds_sdgs: {
+        type: DataTypes.STRING,
+      },
+      ds_sdgs: {
         type: DataTypes.STRING,
       },
       createdAt: {
--- domain/user.js
+++ domain/user.js
@@ -1,8 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    ds_profile: DataTypes.STRING,
+    ds_email: DataTypes.STRING,
+    ds_name: DataTypes.STRING,
+    ds_bio: DataTypes.STRING,
+    dt_birthday: DataTypes.DATE,
+    ds_avatar: DataTypes.STRING,
+    ds_country: DataTypes.STRING,
+    ds_city: DataTypes.STRING,
+    ds_address: DataTypes.STRING,
+    nr_latlong: DataTypes.STRING,
+    ds_skills: DataTypes.STRING,
+    ds_causes: DataTypes.STRING,
+    ds_areas: DataTypes.STRING,
+    ds_sdgs: DataTypes.STRING,
+    fl_is_allow_to_remote: DataTypes.BOOLEAN,
   });
 
   return User;
